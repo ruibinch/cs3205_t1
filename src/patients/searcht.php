436f2ab135ffdbfc373f80a0696aa3e85b0351a5
shawnclq <shawnclq@users.noreply.github.com>
2017-09-18 17:01:23 +0800

    Add files via upload

---
+++ src/patients/searcht.php
@@ -0,0 +1,23 @@
+<?php
+	$therapist = "Chris Evans";
+?>
+
+<html>
+<meta charset="utf-8">
+
+  <head>
+    <title>Edit your profile</title>
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+
+  <body>
+  	<?php include 'sidebar.php' ?>
+	<div class="shifted">
+    <h1><?php echo $therapist ?></h1>
+    <hr style="margin-top:-15px">
+
+	</div>
+  </body>
+
+
+</html>
