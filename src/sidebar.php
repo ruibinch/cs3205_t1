d7190feadf505a6fd995d3bae6085e4066436b97
shawnclq <shawnclq@users.noreply.github.com>
2017-09-18 16:42:09 +0800

    Add sidebar

---
+++ src/sidebar.php
@@ -0,0 +1,15 @@
+<?php
+echo
+	'<div class="sidebar">
+		Menu<br>
+		<form>
+  			<input type="text" name="search" placeholder="Search..">
+		</form>
+		<a href="main.php" style="text-decoration:none">Home</a><br>
+		<a href="viewdoc.php" style="text-decoration:none">Records</a><br>
+		<a href="update.php" style="text-decoration:none">Profile</a><br>
+		<a href="managet.php" style="text-decoration:none">Therapists</a><br>
+		<a href="login.php" style="text-decoration:none">Logout</a><br>
+		<a href="searcht.php" style="text-decoration:none">Search</a><br>
+	</div>'
+?>
