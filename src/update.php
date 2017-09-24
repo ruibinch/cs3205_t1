31b35433f900ea81dc1f214dd38cc11c6aeb469d
shawnclq <shawnclq@users.noreply.github.com>
2017-09-24 14:45:59 +0800

    Update update.php
    Change in colour scheme and layout

--- src/update.php
+++ src/update.php
@@ -20,17 +20,24 @@
 	<div class="shifted">
     <h1>Edit your profile</h1>
     <hr style="margin-top:-15px">
-    <form class="form-profile" name="form-profile" method="post" action="main.php">
-      Username: <input name="input-username" type="text" placeholder="<?php echo $user ?>"><br>
-      Password: <input name="input-password" type="password" placeholder="Password"><br>
-      First Name: <input name="input-firstname" type="text" placeholder="<?php echo $first_name ?>"><br>
-      Last Name: <input name="input-lastname" type="text" placeholder="<?php echo $last_name ?>"><br>
-      Date of Birth: <input type="date" name="input-bday"><br>
-      Phone: <input type="text" name="input-phone"><br>
-      Address: <input type="text" name="input-address"><br>
-      Zipcode: <input type="text" name="input-zipcode"><br>
+    <form class="profile-form" name="profile-form" method="post" action="main.php">
+      <div class="profile-update">Username:<br><input name="input-username" type="text" placeholder="<?php echo $user ?>"><br></div>
 
-      <input type="submit" id="btn-login" name="login" class="btn-login" value="Save">
+      <div class="profile-update">Password:<br><input name="input-password" type="password" placeholder="Password"><br></div>
+
+      <div class="profile-update">First Name:<br><input name="input-firstname" type="text" placeholder="<?php echo $first_name ?>"><br></div>
+
+      <div class="profile-update">Last Name:<br><input name="input-lastname" type="text" placeholder="<?php echo $last_name ?>"><br></div>
+
+      <div class="profile-update">Date of Birth:<br><input type="date" name="input-bday"><br></div>
+
+      <div class="profile-update">Phone:<br><input type="text" name="input-phone"><br></div>
+
+      <div class="profile-update">Address:<br><input type="text" name="input-address"><br></div>
+
+      <div class="profile-update">Zipcode:<br><input type="text" name="input-zipcode"><br></div>
+
+      <div class="profile-update"><input class="profile-submit" type="submit" id="btn-login" name="login" class="btn-login" value="Save"></div>
     </form>
 	</div>
   </body>
