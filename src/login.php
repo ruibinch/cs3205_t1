2e8d9763ed8b0d7e7df54617966e18de5fbf4af6
shawnclq <shawnclq@users.noreply.github.com>
2017-09-24 14:40:12 +0800

    Update login.php

--- src/login.php
+++ src/login.php
@@ -6,7 +6,7 @@
   	  $loginSystem = "Management Console";
   	}
   }
-
+  
 ?>
 
 <html>
@@ -17,25 +17,51 @@
     <link href="css/main.css" rel="stylesheet">
   </head>
 
-  <body>
+  <body class="login">
+    <!-- <h1 style="text-align:center">Login to <?php echo $loginSystem ?></h1> -->
+    <h1>
+      <font size="30px" face="Lucida Grande" color="white">CS3205 Team 1</font><br>
+      <font size="5px" face="Lucida Grande" color="white"><?php echo strtoupper($loginSystem) ?></font>
+    </h1>
+    <div class="login-container">
+      <div class="login-tab">
+        <button class="tablinks user-tab active" onclick="openTab(event, 'User')">User</button>
+        <button class="tablinks mgmt-tab" onclick="openTab(event, 'Management')">Management</button>
 
-    <h1>Login to <?php echo $loginSystem ?></h1>
-    <div class="container">
-      <form class="form-signin" name="form" method="post">
-        <input id="input-username" name="input-username" type="text" class="form-control" placeholder="Username" autofocus>
-        <input id="input-password" name="input-password" type="password" class="form-control" placeholder="Password">
+        <div id="User" class="login-tabcontent" style="display: block;">
+          <form class="login-form" id="form-user" name="form-user" method="post" action="main.php">
+            <input id="username" name="username" type="text" class="login-input" placeholder="Username" autofocus>
+            <input id="password" name="password" type="password" class="login-input" placeholder="Password">
+            <button name="user_type" value="patient" id="btn-login-patient" class="login-btn login-btn-patient">Login as Patient</button>
+            <button name="user_type" value="therapist" id="btn-login-therapist" class="login-btn login-btn-therapist">Login as Therapist</button>
+          </form>
+        </div>
 
-        <button id="btn-login" name="login" class="btn-login">Login</button>
-      </form>
-
-      <?php if ($loginSystem == "Healthcare System") { ?>
-        <form class="form-signin-testing" name="form-testing" method="post" action="main.php">
-          <p><b>For testing</b></p>
-          <button name="user_type" value="patient" id="btn-login-patient" class="btn-login btn-login-patient">Login as Patient</button>
-          <button name="user_type" value="therapist" id="btn-login-therapist" class="btn-login btn-login-therapist">Login as Therapist</button>
-        </form>
-      <?php } ?>
+        <div id="Management" class="login-tabcontent">
+          <form class="login-form" name="form-mgmt" method="post">
+            <input id="mgmt-username" name="mgmt-username" type="text" class="login-input" placeholder="Username" autofocus>
+            <input id="mgmt-password" name="mgmt-password" type="password" class="login-input" placeholder="Password">
+            <button id="btn-login" name="login" class="login-btn login-btn-mgmt">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
+
+    <script>
+    function openTab(evt, tabName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("login-tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+    </script>
 
   </body>
 </html>
