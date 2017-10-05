bf9202eb7f6a6fb2e3961ab5d3827716236661a9
LoneGunman001 <cwm20071128@gmail.com>
2017-10-06 04:18:10 +0800

    finalise jwt and add test for jwt

---
+++ src/util/jwt.php
@@ -0,0 +1,109 @@
+<?php
+require '../composer/vendor/autoload.php';
+use \Firebase\JWT\JWT;
+
+class WebToken
+{
+
+    private static $serverurl = "http://172.25.76.76/";
+
+    /*
+     * @param $uid
+     * @param $key - secret key
+     */
+    static function getToken($uid, $key)
+    {
+        if (self::refreshSecret($uid)) {
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, self::$serverurl."api/team1/user/secret/".$uid);
+            curl_setopt($curl, CURLOPT_PORT , 80);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $secret = json_decode(curl_exec($curl))->secret;
+            $token = array(
+                "exp" => time() + 3600,
+                "secret" => $secret,
+                "data" => array(
+                    "uid" => $uid
+                )
+            );
+            return JWT::encode($token, $key);
+        } else {
+            throw new Exception('Fail to update secret');
+        }
+    }
+
+    /*
+     * @param $token - token found in cookie
+     * @param $key - secret key
+     */
+    static function verifyToken($token, $key)
+    {
+        if ($token) {
+            try {
+                $decoded = JWT::decode($token, "dummy_key", array(
+                    'HS256'
+                ));
+                if ($decoded->exp < time()) {
+                    header("Location: /login.php?error=1");
+                    die();
+                } else {
+                    $curl = curl_init();
+                    curl_setopt($curl, CURLOPT_URL, self::$serverurl . "api/team1/user/secret/" . $decoded->data->uid);
+                    curl_setopt($curl, CURLOPT_PORT, 80);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                    $result = json_decode(curl_exec($curl));
+                    if (strcmp($result->secret, $decoded->secret) != 0) {
+                        /*
+                         * Possile cause: logout, login somewhere else, change psw
+                         */
+                        header("Location: /login.php?error=2");
+                        die();
+                    }
+                    return json_encode($decoded->data);
+                }
+            } catch (Exception $e) {
+                /*
+                 * This token cannot be decoded.
+                 * The signature cannot be verified.
+                 */
+                header("Location: /login.php?error=3");
+                die();
+            }
+        } else {
+            /*
+             * Token is not present.
+             */
+            header("Location: /login.php?error=4");
+            die();
+        }
+    }
+
+    /*
+     * @param $uid
+     * @return true for success, false otherwise
+     */
+    static function refreshSecret($uid)
+    {
+        $string = bin2hex(random_bytes(20));
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, self::$serverurl . "api/team1/user/secret/set/" . $uid . "/" . $string);
+        curl_setopt($curl, CURLOPT_PORT, 80);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = json_decode(curl_exec($curl));
+        if ($result->result == 1)
+            return true;
+        else
+            return false;
+    }
+    
+    static function getSecret($uid)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, self::$serverurl."api/team1/user/secret/3");
+        curl_setopt($curl, CURLOPT_PORT , 80);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = json_decode(curl_exec($curl));
+        return $result->secret;
+    }
+}
+?>
--- src/util/jwttest.php
+++ src/util/jwttest.php
@@ -1,42 +1,19 @@
 <?php
 require '../composer/vendor/autoload.php';
 use \Firebase\JWT\JWT;
+include_once 'jwt.php';
 
-$key = "example_key";
-$token = array(
-    "iss" => "http://example.org",
-    "aud" => "http://example.com",
-    "iat" => 1356999524,
-    "nbf" => 1357000000
-);
+$serverurl = "http://172.25.76.76/";
 
-/**
- * IMPORTANT:
- * You must specify supported algorithms for your application. See
- * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
- * for a list of spec-compliant algorithms.
- */
-$jwt = JWT::encode($token, $key);
-$decoded = JWT::decode($jwt, $key, array('HS256'));
+//test secret
+$result = WebToken::getSecret(3);
+echo 'secret before for uid 3: '.$result."<br>\n";
+WebToken::refreshSecret(3);
+$result = WebToken::getSecret(3);
+echo 'secret after for uid 3: '.$result."<br>\n";
 
-print_r($decoded);
-
-/*
- NOTE: This will now be an object instead of an associative array. To get
- an associative array, you will need to cast it as such:
-*/
-
-$decoded_array = (array) $decoded;
-print_r($decoded_array);
-
-/**
- * You can add a leeway to account for when there is a clock skew times between
- * the signing and verifying servers. It is recommended that this leeway should
- * not be bigger than a few minutes.
- *
- * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
- */
-JWT::$leeway = 60; // $leeway in seconds
-$decoded = JWT::decode($jwt, $key, array('HS256'));
+//test generate
+$token = WebToken::getToken(3, "dummy_key");
+echo "verified token: ".WebToken::verifyToken($token, "dummy_key")."<br>\n";
 
 ?>
