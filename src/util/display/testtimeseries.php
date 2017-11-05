7de757f13a58e8180f832083e1f85f7670947215
LoneGunman001 <cwm20071128@gmail.com>
2017-11-06 01:15:40 +0800

    Remove test php

--- src/util/display/testtimeseries.php
+++
@@ -1,9 +0,0 @@
-<div id = "mydiv"></div>
-
-<?php 
-global $fileurl;
-global $jsonurl;
-$fileurl = "/tmp/RawData_2015.12.16_09.55.28.csv";
-$jsonurl = "/tmp/RawData_2015.12.16_09.55.28.json";
-include 'timeseries.php';
-?>
